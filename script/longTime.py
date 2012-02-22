7f8baa6dc7d1ed5e40054de6ae10e6d68cf17718
liyonghelpme <liyonghelpme@gmail.com>
2012-02-23 04:55:43 +0800

    long time clear empty and operational

--- script/longTime.py
+++ script/longTime.py
@@ -7,7 +7,10 @@ begin = time.mktime(begin)
 now = time.mktime(time.localtime())
 now -= begin
 
-bon = int(now - 24*3600*60)
+bon = int(now - 24*3600*30)
+
+con = None
+cursor = None
 
 def exe(sql):
     print sql
@@ -17,6 +20,8 @@ while True:
     con = MySQLdb.connect(host='localhost', user='root', passwd='badperson3', db = 'stcHong', charset = 'utf8')
     cursor = con.cursor()
     s = 'update operationalData set empirename = \'empty\', infantrypower = 0, cavalrypower = 0, defencepower = 0, logintime = '+str(bon) + ' where userid != 2800 and logintime < ' + str(bon)
+    exe(s)
+    s = 'update emptyCastal set uid = -1, inf = 0, cav = 0, lasttime = '+str(bon)+ ' where lasttime < '+str(bon)
     exe(s)
     time.sleep(3600*24)
 
