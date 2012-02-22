f58bb55bd3379c3ace60d02f339ab27036e24a3c
liyonghelpme <liyonghelpme@gmail.com>
2012-02-23 04:57:12 +0800

    update now time with

--- script/longTime.py
+++ script/longTime.py
@@ -4,10 +4,7 @@ import MySQLdb
 
 begin = [2011, 1, 1, 0, 0, 0, 0, 0, 0]
 begin = time.mktime(begin)
-now = time.mktime(time.localtime())
-now -= begin
 
-bon = int(now - 24*3600*30)
 
 con = None
 cursor = None
@@ -17,6 +14,10 @@ def exe(sql):
     cursor.execute(sql)
     con.commit()
 while True:
+    now = time.mktime(time.localtime())
+    now -= begin
+    bon = int(now - 24*3600*30)
+
     con = MySQLdb.connect(host='localhost', user='root', passwd='badperson3', db = 'stcHong', charset = 'utf8')
     cursor = con.cursor()
     s = 'update operationalData set empirename = \'empty\', infantrypower = 0, cavalrypower = 0, defencepower = 0, logintime = '+str(bon) + ' where userid != 2800 and logintime < ' + str(bon)
