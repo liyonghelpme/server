d5a3059fab5f50ebc8257bd1014b1cb6a3e5b121
liyonghelpme <liyonghelpme@gmail.com>
2012-03-19 21:00:38 +0800

    Dragon disk

--- stchong/controllers/goods.py
+++ stchong/controllers/goods.py
@@ -36,32 +36,112 @@ class GoodsController(BaseController):
     #[[oid, num]]
     #0 - 8
     global Possible
-    Possible = [1, 1, 20, 30, 20, 10, 5, 5, 8]
-    def getReward(self, user, kind):
+    Possible = [
+    [1, 30, 200, 50, 100, 200, 200, 200, 19],
+    [10, 4, 1, 10, 20, 20, 15, 10, 10],
+    [200, 50, 9, 40, 200, 200, 100, 200, 1]
+    ]
+    def getReward(self, user, kind, diskType):
+        if diskType == 0:
+            if kind == 0:
+                mana = DBSession.query(model.Mana).filter_by(userid=user.userid).one()
+                mana.boundary += 1
+            elif kind == 1:
+                user.cae += 9
+            elif kind == 2:
+                user.food += 99
+            elif kind == 3:
+                spe = getSpecial(user)
+                spe[0][1] += 1
+                user.specialgoods = setSpecial(spe)
+            elif kind == 4:
+                user.corn += 999
+            elif kind == 5:
+                changeGoods(user.userid, 0, 1)
+            elif kind == 6:
+                user.food += 999
+            elif kind == 7:
+                user.cae += 1
+            elif kind == 8:
+                user.corn += 9999
+        elif diskType == 1:
+            if kind == 0:
+                user.corn += 99
+            elif kind == 1:
+                user.food += 999
+            elif kind == 2:
+                user.infantrypower += 999
+            elif kind == 3:
+                user.corn += 9
+            elif kind == 4:
+                user.food += 99
+            elif kind == 5:
+                user.infantrypower += 9
+            elif kind == 6:
+                user.corn += 999
+            elif kind == 7:
+                user.food += 9
+            elif kind == 8:
+                user.infantrypower += 99
+        else:
+            if kind == 0:
+                user.corn += 99
+            elif kind == 1:
+                user.food += 999
+            elif kind == 2:
+                user.cae += 9
+            elif kind == 3:
+                user.corn += 9999
+            elif kind == 4:
+                user.food += 99
+            elif kind == 5:
+                user.cae += 1
+            elif kind == 6:
+                user.corn += 999
+            elif kind == 7:
+                user.food += 9
+            elif kind == 8:
+                user.cae += 99
+
+        
+    @expose('json')
+    def startDragon(self, uid, kind):
+        kind = int(kind)
+        uid = int(uid)
+        diskType = kind/1000
+        kind = kind % 1000
+        user = getUser(uid)
+        ok = False
         if kind == 0:
-            mana = DBSession.query(model.Mana).filter_by(userid=user.userid).one()
-            mana.boundary += 1
-        elif kind == 1:
-            user.cae += 9
-        elif kind == 2:
-            user.food += 99
-        elif kind == 3:
-            spe = getSpecial(user)
-            spe[0][1] += 1
-            user.specialgoods = setSpecial(spe)
-        elif kind == 4:
-            user.corn += 999
-        elif kind == 5:
-            changeGoods(user.userid, 0, 1)
-        elif kind == 6:
-            user.food += 999
-        elif kind == 7:
-            user.cae += 1
-        elif kind == 8:
-            user.corn += 9999
-        
-        
-        
+            goods = getGoods(uid).get('goods')
+            stone = goods.get(str(0), 0)
+            #print goods, stone
+            if stone >= 1:
+                ok = True
+                changeGoods(uid, 0, -1)
+            print "startDragon", uid, kind, time.mktime(time.localtime())
+        else:
+            if user.cae >= 1:
+                ok = True
+                user.cae -= 1
+            print "startDragon", uid, kind, time.mktime(time.localtime())
+
+        if ok:
+            all = sum(Possible[diskType])
+            v = random.randint(0, all-1)            
+            last = 0
+            for i in range(len(Possible[diskType])):
+                cur = sum(Possible[diskType][:i+1])
+                if v >= last and v < cur:
+                    break
+                last = cur
+            self.getReward(user, i, diskType)
+            print "dragonReward", uid, i
+            return dict(id=1, result=i)
+        return dict(id=0, reason = "stone or cae not enough")
+
+    #0 normal 1 cae 2 battle 
+    """
     @expose('json')
     def startDragon(self, uid, kind):
         kind = int(kind)
@@ -95,5 +175,4 @@ class GoodsController(BaseController):
             print "dragonReward", uid, i
             return dict(id=1, result=i)
         return dict(id=0, reason = "stone or cae not enough")
-            
-
+    """     
