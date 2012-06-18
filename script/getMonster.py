b5adbdd7d016ae6bd4e23c17fc20499676d6fcc4
liyonghelpme <liyonghelpme@gmail.com>
2012-06-18 09:42:08 +0800

    getMonste

--- script/getMonster.py
+++ script/getMonster.py
@@ -3,7 +3,7 @@ import MySQLdb
 import random
 
 try:
-    conn = MySQLdb.connect(host='localhost',user='root',passwd='badperson3',db='stcHong')
+    conn = MySQLdb.connect(host='localhost',user='root',passwd='2e4n5k2w2x',db='stcHong')
 except Exception, e:
     print e
 
