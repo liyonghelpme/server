69236fa7150dc18744cf21f98df05b75ac69109a
root <root@ip-10-68-55-40.ec2.internal>
2012-07-14 07:47:40 +0000

    dib

--- script/removeRank.py
+++ script/removeRank.py
@@ -1,6 +1,8 @@
 import time
 import pymongo
 
-con = pymongo.Connection(host='localhost', port=27017)
-db = con['Rank']
-db.rankYet.remove()
+while True:
+    con = pymongo.Connection(host='localhost', port=27017)
+    db = con['Rank']
+    db.rankYet.remove()
+    time.sleep(24*3600)
